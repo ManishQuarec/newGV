dd8022bdbf6c63aab65802f223d1ddafc655988d
Manish <manish.quarec@gmail.com>
2023-04-13 18:41:09 +0530

    new

--- components/Video/video.js
+++ components/Video/video.js
@@ -1,10 +1,172 @@
 import React from 'react'
 import styles from "../../styles/Video.module.css"
 import Image from 'next/image'
+import Play from "../../public/play.svg"
 
 function video() {
   return (
-    <div className={styles.Tagbody}>Data</div>
+    <div className={styles.Tagbody}>
+      <div className={styles.VideoBoundary}>
+        <div className={styles.ImageBoundary}>
+          <div className={styles.ImgBoundaries}>
+            <Image
+              src="/imggg.png"
+              alt="My Image"
+              width={222}
+              height={395}
+            />
+            <div className={styles.ef624bc1}><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path fill="white" d="m15 12.33l-6 4.33V8l6 4.33Z" /></svg></div>
+            <div className={styles.textcontent}><span>adta name are wjccnsdc ncas cascb scnascas asncasc</span> :<span >adta name are wjccnsdc ncas cascb scnascas asncasc</span>
+              <div className={styles.footer}> <span className={styles.Category}>data</span> <span className={styles.DateTime}>data</span></div>
+            </div>
+          </div>
+        </div>
+
+        {/* ----------------------------------- */}
+
+        <div className={styles.ImageBoundary}>
+          <div className={styles.ImgBoundaries}>
+            <Image
+              src="/imggg.png"
+              alt="My Image"
+              width={222}
+              height={395}
+            />
+            <div className={styles.ef624bc1}><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path fill="white" d="m15 12.33l-6 4.33V8l6 4.33Z" /></svg></div>
+            <div className={styles.textcontent}><span>adta name are wjccnsdc ncas cascb scnascas asncasc</span> :<span >adta name are wjccnsdc ncas cascb scnascas asncasc</span>
+              <div className={styles.footer}></div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.ImageBoundary}>
+          <div className={styles.ImgBoundaries}>
+            <Image
+              src="/imggg.png"
+              alt="My Image"
+              width={222}
+              height={395}
+            />
+            <div className={styles.ef624bc1}><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path fill="white" d="m15 12.33l-6 4.33V8l6 4.33Z" /></svg></div>
+            <div className={styles.textcontent}><span>adta name are wjccnsdc ncas cascb scnascas asncasc</span> :<span >adta name are wjccnsdc ncas cascb scnascas asncasc</span>
+              <div className={styles.footer}></div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.ImageBoundary}>
+          <div className={styles.ImgBoundaries}>
+            <Image
+              src="/imggg.png"
+              alt="My Image"
+              width={222}
+              height={395}
+            />
+            <div className={styles.ef624bc1}><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path fill="white" d="m15 12.33l-6 4.33V8l6 4.33Z" /></svg></div>
+            <div className={styles.textcontent}><span>adta name are wjccnsdc ncas cascb scnascas asncasc</span> :<span >adta name are wjccnsdc ncas cascb scnascas asncasc</span>
+              <div className={styles.footer}></div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.ImageBoundary}>
+          <div className={styles.ImgBoundaries}>
+            <Image
+              src="/imggg.png"
+              alt="My Image"
+              width={222}
+              height={395}
+            />
+            <div className={styles.ef624bc1}><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path fill="white" d="m15 12.33l-6 4.33V8l6 4.33Z" /></svg></div>
+            <div className={styles.textcontent}><span>adta name are wjccnsdc ncas cascb scnascas asncasc</span> :<span >adta name are wjccnsdc ncas cascb scnascas asncasc</span>
+              <div className={styles.footer}></div>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.ImageBoundary}>
+          <div className={styles.ImgBoundaries}>
+            <Image
+              src="/imggg.png"
+              alt="My Image"
+              width={222}
+              height={395}
+            />
+            <div className={styles.ef624bc1}><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path fill="white" d="m15 12.33l-6 4.33V8l6 4.33Z" /></svg></div>
+            <div className={styles.textcontent}><span>adta name are wjccnsdc ncas cascb scnascas asncasc</span> :<span >adta name are wjccnsdc ncas cascb scnascas asncasc</span>
+              <div className={styles.footer}></div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className={styles.ImageBoundary}>
+          <div className={styles.ImgBoundaries}>
+            <Image
+              src="/imggg.png"
+              alt="My Image"
+              width={222}
+              height={395}
+            />
+            <div className={styles.ef624bc1}><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path fill="white" d="m15 12.33l-6 4.33V8l6 4.33Z" /></svg></div>
+            <div className={styles.textcontent}><span>adta name are wjccnsdc ncas cascb scnascas asncasc</span> :<span >adta name are wjccnsdc ncas cascb scnascas asncasc</span>
+              <div className={styles.footer}></div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className={styles.ImageBoundary}>
+          <div className={styles.ImgBoundaries}>
+            <Image
+              src="/imggg.png"
+              alt="My Image"
+              width={222}
+              height={395}
+            />
+            <div className={styles.ef624bc1}><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path fill="white" d="m15 12.33l-6 4.33V8l6 4.33Z" /></svg></div>
+            <div className={styles.textcontent}><span>adta name are wjccnsdc ncas cascb scnascas asncasc</span> :<span >adta name are wjccnsdc ncas cascb scnascas asncasc</span>
+              <div className={styles.footer}></div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className={styles.ImageBoundary}>
+          <div className={styles.ImgBoundaries}>
+            <Image
+              src="/imggg.png"
+              alt="My Image"
+              width={222}
+              height={395}
+            />
+            <div className={styles.ef624bc1}><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path fill="white" d="m15 12.33l-6 4.33V8l6 4.33Z" /></svg></div>
+            <div className={styles.textcontent}><span>adta name are wjccnsdc ncas cascb scnascas asncasc</span> :<span >adta name are wjccnsdc ncas cascb scnascas asncasc</span>
+              <div className={styles.footer}></div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className={styles.ImageBoundary}>
+          <div className={styles.ImgBoundaries}>
+            <Image
+              src="/imggg.png"
+              alt="My Image"
+              width={222}
+              height={395}
+            />
+            <div className={styles.ef624bc1}><svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path fill="white" d="m15 12.33l-6 4.33V8l6 4.33Z" /></svg></div>
+            <div className={styles.textcontent}><span>adta name are wjccnsdc ncas cascb scnascas asncasc</span> :<span >adta name are wjccnsdc ncas cascb scnascas asncasc</span>
+              <div className={styles.footer}></div>
+            </div>
+          </div>
+        </div>
+
+        {/* --------------------------------- */}
+
+      </div>
+    </div>
   )
 }
 
