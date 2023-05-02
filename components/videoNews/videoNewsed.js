224dab3b3267a90b9e6032dacda8a5ca93242336
RaviQuarec <ravi.quarec@gmail.com>
2023-05-02 11:50:56 +0530

    Video Responsive set

--- components/videoNews/videoNewsed.js
+++ components/videoNews/videoNewsed.js
@@ -2,54 +2,39 @@ import React, { useEffect, useState } from "react";
 import styles from "../../styles/Videonews.module.css";
 import axios from "axios";
 import TimeAgo from "../Timeago/Timeago";
-import Image from 'next/image';
-import { Icon } from '@iconify/react';
-import facebookFill from '@iconify/icons-ri/facebook-fill';
-import twitterIcon from '@iconify/icons-mdi/twitter';
-import linkSimpleBold from '@iconify/icons-ph/link-simple-bold';
-import roundClose from '@iconify/icons-ic/round-close';
-
-
-
+import Image from "next/image";
+import { Icon } from "@iconify/react";
+import facebookFill from "@iconify/icons-ri/facebook-fill";
+import twitterIcon from "@iconify/icons-mdi/twitter";
+import linkSimpleBold from "@iconify/icons-ph/link-simple-bold";
+import roundClose from "@iconify/icons-ic/round-close";
 
 function videoNewsed(props) {
-
-
-
   const [recived, setRecived] = useState({});
 
-  const [videoSrc, setVideoSrc] = useState('');
+  const [videoSrc, setVideoSrc] = useState("");
 
   const [number, setNumber] = useState(props.data);
 
   console.log("propsdata", videoSrc);
 
-
-
   useEffect(() => {
-
     axios
-      .get(process.env.NEXT_PUBLIC_API_BASE_URL + "/getAllVideoData",).then(async (response) => {
+      .get(process.env.NEXT_PUBLIC_API_BASE_URL + "/getAllVideoData")
+      .then(async (response) => {
         setVideoSrc(response.data[number].VideoPath);
 
         await setRecived(response.data[number]);
-
-
-      })
-  }, [])
-
-
+      });
+  }, []);
 
   console.log(process.env.NEXT_PUBLIC_API_URL + videoSrc);
 
   return (
     <>
-
-
-
-
       <div className={styles.fullheight}>
-        <div className={styles.header}><div className={styles.readNews }>Read News</div>
+        <div className={styles.header}>
+          <div className={styles.readNews}>Read News</div>
           <div className={`${styles.icon} ${styles.facebook}`}>
             {/* <Image
             style={{color:"green"}}
@@ -59,28 +44,46 @@ function videoNewsed(props) {
               height={40}
             /> */}
             {/* <img src="/facebook-01.svg" alt="An SVG of an eye"  style={{color:"green"}}/> */}
-            <Icon style={{height:"31px", width:"38px", marginTop:"10%"}} icon={facebookFill} className={styles.facebookicon} />
-           
+            <Icon
+              style={{ height: "31px", width: "38px", marginTop: "10%" }}
+              icon={facebookFill}
+              className={styles.facebookicon}
+            />
           </div>
           <div className={`${styles.icon} ${styles.twitter}`}>
-          
-          <Icon style={{height:"31px", width:"38px", marginTop:"10%"}} icon={twitterIcon} className={styles.twitterIcon} />
-           
+            <Icon
+              style={{ height: "31px", width: "38px", marginTop: "10%" }}
+              icon={twitterIcon}
+              className={styles.twitterIcon}
+            />
           </div>
           <div className={`${styles.icon} ${styles.link}`}>
-          
-          <Icon style={{height:"31px", width:"38px", marginTop:"10%"}} icon={linkSimpleBold} className={styles.linkSimple} />
-         
-        </div>
-        <div className={`${styles.icon} ${styles.link}`}>
-          
-          <Icon style={{height:"31px", width:"38px", marginTop:"10%"}}  icon={roundClose} className={styles.linkSimple} />
-         
-        </div>
+            <Icon
+              style={{ height: "31px", width: "38px", marginTop: "10%" }}
+              icon={linkSimpleBold}
+              className={styles.linkSimple}
+            />
+          </div>
+          <div className={`${styles.icon} ${styles.link}`}>
+            <Icon
+              style={{ height: "31px", width: "38px", marginTop: "10%" }}
+              icon={roundClose}
+              className={styles.linkSimple}
+            />
+          </div>
         </div>
 
         <div className={styles.videoborder}>
-          <video src={process.env.NEXT_PUBLIC_API_URL + videoSrc} height="200" width="500" autoPlay controls disablePictureInPicture playsInline controlsList="nodownload" />
+          <video
+            src={process.env.NEXT_PUBLIC_API_URL + videoSrc}
+            height="200"
+            width="500"
+            autoPlay
+            controls
+            disablePictureInPicture
+            playsInline
+            controlsList="nodownload"
+          />
         </div>
       </div>
 
@@ -90,19 +93,14 @@ function videoNewsed(props) {
           <h2 className={styles.sutittle}>{recived.NewsSubTittle}</h2>
           <div className={styles.subfooter}>
             <div className={styles.sudata}>{recived.GujCategory}</div>
-            <div className={styles.sudata}><TimeAgo timestamp={`${recived.CreatedDate}`} /></div>
-
+            <div className={styles.sudata}>
+              <TimeAgo timestamp={`${recived.CreatedDate}`} />
+            </div>
           </div>
-
         </div>
-
       </div>
-
-
     </>
-
   );
-};
+}
 
-
-export default videoNewsed
+export default videoNewsed;
